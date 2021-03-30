eec6bb57a9815d2b1c1a8b13440c20e4a7f3c191
Tyler Nevell <tylernevell@gmail.com>
2021-03-30 12:37:26 -0700

    Add mongoose functionality to todolist. Introduce custom list functionality. Add ability to add list items to custom lists.

--- app.js
+++ app.js
@@ -1,22 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
-const items = [];
-const workItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://n3vdawg:" + process.env.MONGO_PASSWORD + "@fruitcluster.3zp30.mongodb.net/toDoListDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Data Schema
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+///////////////////
+// DEFAULT ITEMS //
+///////////////////
+const item1 = new Item({
+    name: "Welcome to your Todo List!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
 /////////////////
 // HOME ROUTE //
 ///////////////
 app.get("/", (req, res) => {
-    const day = date.getDate();
-    res.render("list", {listTitle: day, newListItems: items});
+    // const day = date.getDate();
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Success.");
+                }
+            });
+        } else {
+            res.render("list", {listTitle: "Today", newListItems: foundItems});
+        }
+    });
+
 });
 
 /////////////////
